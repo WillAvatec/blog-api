d851a6e54d0c224ec9a2d8fb3c58665121b50c57
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-27 23:04:47 -0500

    chore: Create User model

---
+++ server/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "member"],
+    default: "member",
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
