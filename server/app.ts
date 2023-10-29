32b037af3f540347baccef0f35fe28215ea38eaf
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-28 22:26:31 -0500

    chore:  Append postRouter to "/posts" route

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import "dotenv/config";
+import { postRouter } from "./routes";
 // Initialize server
 const app = express();
 const port = process.env.PORT || 3000;
 
 // MultiRoute Middlewares
 app.use(morgan("dev"));
+
+// Router middlewares
+app.use("/posts", postRouter);
 
 app.listen(port, () => {
   console.log("Server ON: Start listening in " + port);
