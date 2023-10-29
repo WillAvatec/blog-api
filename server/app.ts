2cc2ee0215bdf89c32578b17246d3de139d729e6
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-28 20:47:04 -0500

    chore: Listen to port

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,8 @@
 import express from "express";
-
 // Initialize server
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server ON: Start listening in " + port);
+});
