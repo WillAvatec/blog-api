e36c31255f9069813ad88733065ac202ad4b5c6b
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-28 22:23:46 -0500

    chore: Use controllers in post router

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,20 +1,21 @@
 import express from "express";
+import { postController } from "../controllers";
 
 const router = express.Router();
 
 // Return all the posts
-router.get("/");
+router.get("/", postController.posts_get_all);
 
 // Return one specific post
-router.get("/:id");
+router.get("/:id", postController.posts_get_one);
 
 // Push new post to database
-router.post("/");
+router.post("/", postController.posts_new);
 
 // Update one Post
-router.put("/:id");
+router.put("/:id", postController.posts_put);
 
 // Erase one specific post from database
-router.delete("/:id");
+router.delete("/:id", postController.posts_delete);
 
 export default router;
