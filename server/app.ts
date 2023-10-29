cbc9db96507856106e707808bd45948abee49990
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-28 20:47:42 -0500

    chore: Add morgan to all the routes

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 // Initialize server
 const app = express();
 const port = process.env.PORT || 3000;
+
+// MultiRoute Middlewares
+app.use(morgan("dev"));
 
 app.listen(port, () => {
   console.log("Server ON: Start listening in " + port);
