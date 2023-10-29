2e458bf893f42514b8c22fbb76b79a2b96cbab25
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-28 21:04:51 -0500

    chore: Predefine posts routes

---
+++ server/routes/posts.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const router = express.Router();
+
+// Return all the posts
+router.get("/");
+
+// Return one specific post
+router.get("/:id");
+
+// Push new post to database
+router.post("/");
+
+// Update one Post
+router.put("/:id");
+
+// Erase one specific post from database
+router.delete("/:id");
+
+export default { postRouter: router };
