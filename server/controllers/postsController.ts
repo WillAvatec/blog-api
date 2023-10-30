bcaa56865f23b30cf1cc923bf33262298e8edadf
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-29 19:37:01 -0500

    refact: Wrap controllers inside async catch func

--- server/controllers/postsController.ts
+++ server/controllers/postsController.ts
@@ -1,20 +1,24 @@
-import { Request, Response } from "express";
 import { Post } from "../models";
+import asyncHand from "express-async-handler";
 
 const postsController = (() => {
-  const posts_get_all = async (req: Request, res: Response) => {
+  const posts_get_all = asyncHand(async (req, res) => {
     const allPosts = await Post.find().populate("author").exec();
-    return res.json(allPosts);
-  };
 
-  const posts_get_one = async (req: Request, res: Response) => {
+    console.log(allPosts);
+
+    // If there are no posts in db, it will return an empty array
+    res.json(allPosts);
+  });
+
+  const posts_get_one = asyncHand(async (req, res) => {
     const onePost = await Post.findById(req.params.id)
       .populate(["author", "comments"])
       .exec();
-    return res.json(onePost);
-  };
+    res.json(onePost);
+  });
 
-  const posts_new = async (req: Request, res: Response) => {
+  const posts_new = asyncHand(async (req, res) => {
     const newPost = new Post({
       author: req.body.author,
       title: req.body.title,
@@ -22,37 +26,38 @@ const postsController = (() => {
     });
 
     const saved = await newPost.save();
+    res.status(201).json(saved);
+  });
 
-    return res.status(201).json(saved);
-  };
-
-  const posts_put = async (req: Request, res: Response) => {
+  const posts_put = asyncHand(async (req, res) => {
+    // For the find to work,the id must be at least 10+ characters long
     const post = await Post.findById(req.params.id);
     if (post === null) {
       // Resource not found
-      return res.status(404).json({
+      res.status(404).json({
         err: "Resource not found",
       });
+      return;
     }
-
     post.content = req.body.content;
     post.updatedAt = new Date();
 
     const saved = await post.save({ timestamps: true });
-    return res.status(202).json(saved);
-  };
+    res.status(202).json(saved);
+  });
 
-  const posts_delete = async (req: Request, res: Response) => {
+  const posts_delete = asyncHand(async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post === null) {
       // Resource not found
-      return res.status(404).json({
+      res.status(404).json({
         err: "Resource not found",
       });
+      return;
     }
 
-    return res.status(200).end();
-  };
+    res.status(200).end();
+  });
 
   return {
     posts_get_all,
