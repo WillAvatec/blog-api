258d845d48803523c067b393f126e99053aba554
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-01 12:48:37 -0500

    chore: Hash password on modification (or new)

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
   username: {
@@ -14,6 +15,21 @@ const userSchema = new Schema({
     enum: ["admin", "member"],
     default: "member",
   },
+});
+
+const SALT_FACTOR = 10;
+
+// Hash a password on save
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  bcrypt.hash(this.password, SALT_FACTOR, async (err, hashedPassword) => {
+    if (err) return next(err);
+
+    // Override string with the hashed one
+    this.password = hashedPassword;
+    next();
+  });
 });
 
 const User = mongoose.model("user", userSchema);
