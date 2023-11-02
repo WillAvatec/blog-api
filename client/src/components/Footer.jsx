137eeb153b0f3e8ace166839920b0b3eea3f4f3f
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-01 20:45:51 -0500

    style: Fix hover effect on github link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,15 @@
 const Footer = () => {
   return (
     <footer>
-      <p>
-        Developed by{" "}
-        <a
-          href="https://github.com/WillAvatec"
-          target="_blank"
-          rel="noreferrer"
-        >
-          avatec
-        </a>
-      </p>
-      <i className="devicon-github-original devicon"></i>
+      <p>Developed by avatec2312</p>
+      <a
+        className="github-link"
+        href="https://github.com/WillAvatec"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="devicon-github-original devicon"></i>
+      </a>
     </footer>
   );
 };
