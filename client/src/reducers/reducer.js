5d6da080da1624abb26dea6f91bc1f5b2e3e4ad6
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-02 15:47:41 -0500

    feat: New reducer to store input data from sign up

---
+++ client/src/reducers/reducer.js
@@ -0,0 +1,25 @@
+function reducer(state, action) {
+  switch (action.type) {
+    case "update-username": {
+      return {
+        ...state,
+        username: action.newValue,
+      };
+    }
+    case "update-password": {
+      return {
+        ...state,
+        password: action.newValue,
+      };
+    }
+    case "update-confirm": {
+      return {
+        ...state,
+        confirm: action.newValue,
+      };
+    }
+  }
+  throw new Error("Unknown action:" + action.type);
+}
+
+export default reducer;
