e87743245fda81191ea245e1fd6b93b3a04febe6
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-01 21:16:11 -0500

    feat: Router component

---
+++ client/src/components/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+import UserForm from "../pages/UserForm";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/sign-up",
+      element: <UserForm />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
