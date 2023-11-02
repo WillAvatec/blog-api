4d7c6ccfe9590f9101ed8f4489ea16a5a685b6d3
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-01 20:29:35 -0500

    feat: Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Developed by{" "}
+        <a
+          href="https://github.com/WillAvatec"
+          target="_blank"
+          rel="noreferrer"
+        >
+          avatec
+        </a>
+      </p>
+      <i className="devicon-github-original devicon"></i>
+    </footer>
+  );
+};
+
+export default Footer;
