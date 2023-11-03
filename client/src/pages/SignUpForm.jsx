da2d6d93410734c1213aa2a59916ca53b3b8bcd6
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-11-02 21:36:25 -0500

    refact: Rename reduce file

--- client/src/pages/SignUpForm.jsx
+++ client/src/pages/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import reducer from "../reducers/reducer";
+import reducer from "../reducers/signUp";
 import Logo from "../components/Logo";
 import InputField from "../components/Input";
 
